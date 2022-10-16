4267e2418d759b26c4826e78fdb9a48f7032c04f
amirhertz <amirhertz@mail.tau.ac.il>
2022-10-16 22:41:28 +0300

    fluffy example

--- ptp_files/cat_hat.js
+++ ptp_files/cat_hat.js
@@ -51,6 +51,14 @@ function modifyHat(i){
     return inner_
 }
 
+function set_alpha(slider, fluffy){
+    let alpha = 1 - (slider.value - 16) / 52;
+    let red = Math.round(248 + 7 * alpha);
+    let green = Math.round(128 + 127 * alpha);
+    let blue = Math.round(255 * alpha);
+    fluffy.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
 function init() {
     image = document.getElementById("cat-hat-img");
 	animals = document.getElementsByClassName("cat-hat-button_a");
@@ -63,6 +71,17 @@ function init() {
     for (let i = 0; i < hats.length; ++i){
         hats[i].addEventListener("click", modifyHat(i));
     }
+    let slider = document.getElementById("range_bunny");
+    let image_bunny = document.getElementById("bunny-img");
+    let fluffy  = document.getElementById("fluffy");
+    set_alpha(slider, fluffy)
+    slider.oninput = function() {
+        // 248, 128, 0
+     image_bunny.src = "./ptp_files/bunny_seq/fluffy_bunny_" + String(slider.value).padStart(2, '0') + ".png";
+     set_alpha(slider, fluffy)
+
+    }
+
 
 }
 document.addEventListener("DOMContentLoaded", () => {
